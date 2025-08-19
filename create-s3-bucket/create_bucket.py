8d0671a5602570c93858378a7653f52d6e188653
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-08-19 09:40:12 +0800

    Rewrote script for file based input

--- create-s3-bucket/create_bucket.py
+++ create-s3-bucket/create_bucket.py
@@ -2,28 +2,29 @@ import boto3,botocore,json,string,random,pprint
 
 session = boto3.Session(profile_name='plasabas-general-admin')
 client  = session.client('s3')
-buckets = ['plasabas-bucket','plasabas-project-bucket-01']
-length_of_suffix = 8
-random_suffix = ''.join(random.choices(string.ascii_lowercase + string.digits, k=length_of_suffix))
+list_of_buckets = client.list_buckets()["Buckets"]
 
 def main():
-    result = create_bucket()
-    return result
+    list_of_empty_buckets = []
 
-def create_bucket():
-    created_bucket_response=[]
-    for bucket in buckets:
-        response = client.create_bucket(
-            Bucket = f"{bucket}-{random_suffix}"
-        )
-        created_bucket_response.append(response)
-    return created_bucket_response
+    print(f"{'='*100}\nList of Empty buckets bucket\n{'='*100}")
+    for bucket in list_of_buckets:
+        check_if_empty = client.list_objects_v2(Bucket=bucket["Name"])
+        if check_if_empty["KeyCount"] == 0:
+            list_of_empty_buckets.append(bucket["Name"])
+            print(f"{bucket["Name"]}")
+    print(f"{'='*100}\nEnd of the list\n{'='*100}")
+
+    return list_of_empty_buckets
+
+def delete_empty_buckets(list_of_empty_buckets):
+
+    print(f"{'='*100}\nDeleting Empty Buckets\n{'='*100}")
+    for bucket in list_of_empty_buckets:
+        print(f"Deleting: {bucket}")
+        client.delete_bucket(Bucket=bucket)
 
 
 if __name__ == "__main__":
-    result = main()
-
-    for item in result: 
-        bucket_name = item["ResponseMetadata"]["HTTPHeaders"]["location"]
-        print(f"{'='*100}\nCreated {bucket_name.lstrip('/')} bucket\n{'='*100}")
-        print(json.dumps(item, indent=4, default=str))
+    list_of_empty_buckets = main()
+    delete_empty_buckets(list_of_empty_buckets)
